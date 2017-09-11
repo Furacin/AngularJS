6658954b3073f45a0fba4f46044e5bee83dad07c
Francisco Rafael Checa Molina <manzanile@gmail.com>
2017-09-11 15:11:49 +0200

    Añadida imagen en cada iteración

--- app.js
+++ app.js
@@ -9,12 +9,22 @@
         {
             name: 'Dodecahedron',
             price: 2.95,
-            description: 'A good price dude'
+            description: 'A good price dude',
+            images: [
+                {
+                    full: './img/Dodecahedron.svg.png'    
+                }
+            ]
         },
         {
             name: 'Pentagonal Gem',
             price: 5.95,
-            description: 'A good one'
+            description: 'A good one',
+            images: [
+                {
+                    full: './img/Dodecahedron.svg.png'    
+                }
+            ]
         }
     ];
 })();
